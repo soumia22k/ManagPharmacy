b08b5f3f603b8fc8a9c30f74d129d11ea5a77485
soumia <kablaouisoumia75@gmai.com>
2025-05-08 14:32:57 +0100

    La fonction Update , Delete

--- src/main/java/com/Pharmacy/pharmacy/controllers/ProduitController.java
+++ src/main/java/com/Pharmacy/pharmacy/controllers/ProduitController.java
@@ -18,17 +18,27 @@ public class ProduitController {
     }
 
 
-    @PostMapping("/produits")
+    @PostMapping("/produit")
 
 
     public Produit saveProduit(@RequestBody Produit produit) {
         return produitRepository.save(produit);
     }
 
-    @GetMapping("/produit")
+    @GetMapping("/produits")
     public List<Produit> getAllProduits() {
         return produitRepository.findAll();
     }
 
+
+    @DeleteMapping("/produit/{id}")
+    public void deleteProduit(@PathVariable Long id) {
+        produitRepository.deleteById(id);
+    }
+
+    @PutMapping("/produit")
+    public Produit UpdateProduit( @RequestBody Produit produit) {
+        return produitRepository.save(produit);
+    }
 
 }
