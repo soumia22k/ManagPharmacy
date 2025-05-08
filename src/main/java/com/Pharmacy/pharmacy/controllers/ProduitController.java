1e1c9b0b554ca464dd0679936838cd9c417e87a2
soumia <kablaouisoumia75@gmai.com>
2025-05-08 11:51:36 +0100

    le controller

---
+++ src/main/java/com/Pharmacy/pharmacy/controllers/ProduitController.java
@@ -0,0 +1,30 @@
+package com.Pharmacy.pharmacy.controllers;
+
+
+import com.Pharmacy.pharmacy.entities.Produit;
+import com.Pharmacy.pharmacy.repositories.ProduitRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProduitController {
+
+
+    private final ProduitRepository produitRepository;
+
+    public ProduitController(ProduitRepository produitRepository) {
+        this.produitRepository = produitRepository;
+    }
+
+
+    @PostMapping("/produits")
+
+
+    public Produit saveProduit(@RequestBody Produit produit) {
+        return produitRepository.save(produit);
+    }
+
+
+
+}
